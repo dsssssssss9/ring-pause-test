a7a66525b47ece7d63f19157430334fb4421f9a4
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-08-07 17:10:26 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     modules.led1.setBrightness(65)
-    modules.led1.setAll(0xff8000)
+    modules.led1.setAll(0x00ff00)
 })
 input.onButtonPressed(Button.B, function () {
     modules.led1.setBrightness(28)
-    modules.led1.setAll(0x7f00ff)
+    modules.led1.setAll(0x65471f)
 })
 modules.led1.setAll(0x000000)
-modules.led1.setBrightness(65)
+modules.led1.setBrightness(0)
 basic.pause(222)
-basic.forever(function () {
-	
-})
